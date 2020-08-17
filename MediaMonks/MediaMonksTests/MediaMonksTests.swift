4829e0762b0602f9604a7a5da7bf1df1961bfa5b
Marcelo Laprea <marcelojosel15@gmail.com>
2020-08-17 17:07:01 +0200

    Create project

---
+++ MediaMonks/MediaMonksTests/MediaMonksTests.swift
@@ -0,0 +1,34 @@
+//
+//  MediaMonksTests.swift
+//  MediaMonksTests
+//
+//  Created by Marcelo Laprea on 17/08/2020.
+//  Copyright © 2020 Marcelo Laprea. All rights reserved.
+//
+
+import XCTest
+@testable import MediaMonks
+
+class MediaMonksTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
